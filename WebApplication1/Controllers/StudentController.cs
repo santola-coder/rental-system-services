ca2aed9cfe268c52f799cb901db6669c760cfd73
santola.developer@gmail.com <santola.developer@gmail.com>
2026-02-05 14:57:51 +0700

    learn how to make qr code by generation

---
+++ WebApplication1/Controllers/StudentController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Data;
+using WebApplication1.Models.Entities;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public StudentsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            return Ok(_context.Students.ToList());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudentById(int id)
+        {
+            var student = _context.Students.Find(id);
+            if (student == null) return NotFound();
+            return Ok(student);
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent([FromBody] Students student)
+        {
+            _context.Students.Add(student);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetStudentById), new { id = student.id }, student);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateStudent(int id, [FromBody] Students updatedStudent)
+        {
+            var student = _context.Students.Find(id);
+            if (student == null) return NotFound();
+
+            student.name = updatedStudent.name;
+            student.age = updatedStudent.age;
+            student.telephone = updatedStudent.telephone;
+
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudent(int id)
+        {
+            var student = _context.Students.Find(id);
+            if (student == null) return NotFound();
+
+            _context.Students.Remove(student);
+            _context.SaveChanges();
+            return NoContent();
+        }
+    }
+}
---
+++ WebApplication1/Models/Entities/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication1.Models.Entities
+{
+    public class Students
+    {
+        public int id { get; set; }
+
+        public string name { get; set; }
+      
+        public int age { get; set; }
+
+        public string telephone { get; set; }
+    }
+}
